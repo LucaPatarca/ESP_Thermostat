3e9dd94a38f0797da2f71d0c36617fbfb81e7d58
Luca Patarca <luca.patarca@gmail.com>
2024-01-09 17:03:45 +0100

    add auto discovery

--- lib/Remote/MQTT.cpp
+++ lib/Remote/MQTT.cpp
@@ -1,11 +1,15 @@
 #include <MQTT.h>
 #include <sdebug.h>
 
-#define CURRENT_TEMP_GET_TOPIC "esp-thermostat/current-temperature/get"
-#define TEMP_SETPOINT_GET_TOPIC "esp-thermostat/setpoint-temperature/get"
-#define TEMP_SETPOINT_SET_TOPIC "esp-thermostat/setpoint-temperature/set"
-#define MODE_GET_TOPIC "esp-thermostat/mode/get"
-#define MODE_SET_TOPIC "esp-thermostat/mode/set"
+#define CURRENT_TEMP_GET_TOPIC "esp-therm/curr-temp/get"
+#define CURRENT_HUMIDITY_GET_TOPIC "esp-therm/curr-hum/get"
+#define TEMP_SETPOINT_GET_TOPIC "esp-therm/temp/get"
+#define TEMP_SETPOINT_SET_TOPIC "esp-therm/temp/set"
+#define MODE_GET_TOPIC "esp-therm/mode/get"
+#define MODE_SET_TOPIC "esp-therm/mode/set"
+#define MQTT_CONFIG_TOPIC "homeassistant/climate/esp-thermostat/config"
+
+const char MQTT_CONFIG_STRING [] = "{\"name\": \"Termosifoni\", \"uniq_id\": \"esp-therm1\", \"dev_cla\": \"climate\", \"dev\": {\"ids\": [\"esp-therm1\"], \"name\": \"Termosifoni\" }, \"modes\": [ \"off\", \"heat\" ], \"mode_cmd_t\": \"esp-therm/mode/set\", \"mode_stat_t\": \"esp-therm/mode/get\", \"temp_cmd_t\": \"esp-therm/temp/set\", \"temp_stat_t\": \"esp-therm/temp/get\", \"curr_temp_t\": \"esp-therm/curr-temp/get\", \"current_humidity_topic\": \"esp-therm/curr-hum/get\", \"precision\": 0.1}";
 
 MQTTController::MQTTController() : m_state(State::Instance())
 {
@@ -52,38 +56,39 @@ void MQTTController::MQTTCallback(char *topic, byte *payload, unsigned int lengt
 void MQTTController::targetTemperatureChanged(Cause cause)
 {
     sniprintf(m_msgBuf, MSG_BUFFER_SIZE, "%.1f", m_state.getTargetTemperature());
-    if (m_mqttClient.publish(TEMP_SETPOINT_GET_TOPIC, m_msgBuf))
-    {
-        INFO("Send target temperature Ok");
-    }
-    else
+    if (!m_mqttClient.publish(TEMP_SETPOINT_GET_TOPIC, m_msgBuf))
     {
         ERROR("Error sending target temperature");
+        return;
     }
+    INFO("Send target temperature Ok");
 }
 void MQTTController::powerStateChanged(Cause cause)
 {
     sniprintf(m_msgBuf, MSG_BUFFER_SIZE, "%s", m_state.getPowerState() ? "heat" : "off");
-    if (m_mqttClient.publish(MODE_GET_TOPIC, m_msgBuf))
-    {
-        INFO("Send thermostat mode Ok");
-    }
-    else
+    if (!m_mqttClient.publish(MODE_GET_TOPIC, m_msgBuf))
     {
         ERROR("Error sending thermostat mode");
+        return;
     }
+    INFO("Send thermostat mode Ok");
 }
 void MQTTController::currentTemperatureChanged()
 {
-    sniprintf(m_msgBuf, MSG_BUFFER_SIZE, "%.1f", m_state.getCurrentTemperature().temp);
-    if (m_mqttClient.publish(CURRENT_TEMP_GET_TOPIC, m_msgBuf))
-    {
-        INFO("Send current temperature Ok");
-    }
-    else
+    auto temp = m_state.getCurrentTemperature();
+    sniprintf(m_msgBuf, MSG_BUFFER_SIZE, "%.1f", temp.temp);
+    if (!m_mqttClient.publish(CURRENT_TEMP_GET_TOPIC, m_msgBuf))
     {
         ERROR("Error sending current temperature");
+        return;
     }
+    sniprintf(m_msgBuf, MSG_BUFFER_SIZE, "%.1f", temp.humidity);
+    if (!m_mqttClient.publish(CURRENT_HUMIDITY_GET_TOPIC, m_msgBuf))
+    {
+        ERROR("Error sending current humidity");
+        return;
+    }
+    INFO("Send current temperature Ok");
 }
 void MQTTController::thermostatModeChanged(Cause cause) {}
 
@@ -94,6 +99,9 @@ void MQTTController::connect()
     m_mqttClient.setServer(mqttServer, mqttPort);
     m_mqttClient.setCallback([this](char *topic, byte *payload, unsigned int length)
                              { this->MQTTCallback(topic, payload, length); });
+    if(!m_mqttClient.setBufferSize(1024)) {
+        ERROR("Failed to set buffer size");
+    }
     const char *mqttUser =  m_state.getConfig().mqttUser;
     const char *mqttPassword = m_state.getConfig().mqttPassword;
     INFO("Attempting MQTT connection (%s:%d user=%s pass=%s)", mqttServer,
@@ -104,6 +112,9 @@ void MQTTController::connect()
         INFO("MQTT connected");
         m_mqttClient.subscribe(TEMP_SETPOINT_SET_TOPIC);
         m_mqttClient.subscribe(MODE_SET_TOPIC);
+        if(!m_mqttClient.publish(MQTT_CONFIG_TOPIC, MQTT_CONFIG_STRING)) {
+            ERROR("Failed to send configuration, status: %d", m_mqttClient.state());
+        }
         currentTemperatureChanged();
         targetTemperatureChanged(Cause::AUTO);
         powerStateChanged(Cause::AUTO);
